5b85ce9fdf0a04b0041d8e014d05eaebc33d6fe2
Nathan Blaz <nathanblaz@gmail.com>
2021-05-03 13:48:26 -0700

    Login routes complete

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -12,7 +12,7 @@ const userValidators = [
     .withMessage("Please provide a value for First Name")
     .isLength({ max: 100 })
     .withMessage("First Name must not be more than 50 characters long"),
-  check("emailAddress")
+  check("email")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a value for Email Address")
     .isLength({ max: 255 })
@@ -20,7 +20,7 @@ const userValidators = [
     .isEmail()
     .withMessage("Email Address is not a valid email")
     .custom((value) => {
-      return db.User.findOne({ where: { emailAddress: value } }).then(
+      return db.User.findOne({ where: { email: value } }).then(
         (user) => {
           if (user) {
             return Promise.reject(
@@ -98,5 +98,64 @@ router.post(
     }
   })
 );
+
+router.get("/login", csrfProtection, (req, res) => {
+  res.render("user-login", {
+    title: "User Login",
+    csrfToken: req.csrfToken()
+  });
+});
+
+const loginValidators = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Email Address")
+    .isLength({ max: 255 })
+    .withMessage("Email Address must not be more than 255 characters long")
+    .isEmail()
+    .withMessage("Email Address is not a valid email")
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } }).then(
+        (user) => {
+          if (!user) {
+            return Promise.reject(
+              "This email address doesn't exist"
+            );
+          }
+        }
+      );
+    }),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Password"),
+];
+
+router.post("/login", csrfProtection, loginValidators, asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  let errors = [];
+  const validatorErrors = validationResult(req);
+  if (validatorErrors.isEmpty()) {
+    const user = await User.findOne({
+      where: { email }
+    });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+      if (passwordMatch) {
+        loginUser(req, res, user);
+        // TODO persist user data from page to page?
+        res.redirect("/");
+      }
+    }
+    errors.push("Login failed for email and password"); // for security
+  } else {
+    errors = validatorErrors.array().map((error) => error.msg);
+  }
+  res.render("user-login", {
+    title: "User Login",
+    email,
+    errors,
+    csrfToken: req.csrfToken()
+  });
+}));
 
 module.exports = router;
