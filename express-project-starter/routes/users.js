79f5c63aa316b47e43ee82fc8b044f5e158700bf
jdaniel01 <professor4cob@outlook.com>
2021-05-03 10:48:53 -0700

    updated the users routes.

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const {User} = require('../db/models');
+const {check, validationResult} = require('express-validator')
+const {asyncHandler, csrfProtection} = require('./utils');
+const bcrypt = require('bcryptjs');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userValidators = [
+  //TODO Need to set up user validators.
+]
+
+router.get('/register', csrfProtection, (req, res, next) => {
+  const user = User.build();
+  res.render('user-register', {title: 'User Register', csrfToken: req.csrfToken(), user});
 });
+
+router.post('/register', csrfProtection, userValidators, asyncHandler(async (req, res, next) => {
+  const {name, email, bio, password, avatar} = req.body;
+  const user = User.build({name, email, bio, avatar});
+  const validatorErrors = validationResult(req);
+
+  if(validatorErrors.isEmpty()){
+    //TODO hash password.
+  }
+}));
 
 module.exports = router;
